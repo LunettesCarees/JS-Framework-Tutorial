45aac553ece3d9b59c7cdf75bb227f4dfe72e318
Guanzhou Chen <guanzhouchen11@gmail.com>
2024-07-28 05:31:24 -0400

    chore: Update npm dependencies and configuration files

--- api/config/app.config.js
+++ api/config/app.config.js
@@ -1,10 +1,22 @@
+// const STRIPE_CONFIG = {
+//     SECRET_KEY: "sk_test_51NeMVWGtXGgE8I1fyUpTo3P4dF0GAA2cHKjIYALF013cema2BZtajvsYfhFpS0RKRplnYyC8aB0ou2RzMs3BumKl003EhwhNd4",
+//     CURRENCY: "CAD",
+//     SUCCESS_URL: "http://localhost:4200/success?session_id={CHECKOUT_SESSION_ID}",
+//     CANCEL_URL: "http://localhost:4200/payment"
+// };
+
+// module.exports = {
+//     STRIPE_CONFIG
+// };
+
 const STRIPE_CONFIG = {
-    SECRET_KEY: "sk_test_51NeMVWGtXGgE8I1fyUpTo3P4dF0GAA2cHKjIYALF013cema2BZtajvsYfhFpS0RKRplnYyC8aB0ou2RzMs3BumKl003EhwhNd4",
-    CURRENCY: "CAD",
-    SUCCESS_URL: "http://localhost:4200/success?session_id={CHECKOUT_SESSION_ID}",
-    CANCEL_URL: "http://localhost:4200/payment"
+    SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+    CURRENCY: process.env.STRIPE_CURRENCY,
+    SUCCESS_URL: process.env.STRIPE_SUCCESS_URL,
+    CANCEL_URL: process.env.STRIPE_CANCEL_URL
 };
 
 module.exports = {
     STRIPE_CONFIG
 };
+
--- api/index.js
+++ api/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -15,3 +17,35 @@ app.use("/api", require("./routes/app.routes"));
 app.listen(4000, () => {
     console.log("Server is running on port 4000");
 });
+
+// const express = require('express');
+// const app = express();
+// const cors = require('cors');
+
+// // Replace with your frontend URL on Render
+// const frontendURL = 'https://your-frontend-service.onrender.com';
+
+// const corsOptions = {
+//     origin: frontendURL,  // Allow only your frontend domain
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],  // Specify allowed methods
+//     allowedHeaders: ['Content-Type', 'Access-Control-Allow-Origin', 'Origin', 'Authorization'],  // Add any additional headers your application uses
+// };
+
+// app.use(cors(corsOptions));
+// app.use(express.json());
+// app.use("/api", require("./routes/app.routes"));
+
+// app.listen(4000, () => {
+//     console.log("Server is running on port 4000");
+// });
+
+const STRIPE_CONFIG = {
+    SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+    CURRENCY: process.env.STRIPE_CURRENCY,
+    SUCCESS_URL: process.env.STRIPE_SUCCESS_URL,
+    CANCEL_URL: process.env.STRIPE_CANCEL_URL
+};
+
+module.exports = {
+    STRIPE_CONFIG
+};
