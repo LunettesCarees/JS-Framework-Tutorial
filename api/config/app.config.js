e929ef9232a318f5a89644102d0d738e85f74f05
Ryan Chen <113140112+LunettesCarees@users.noreply.github.com>
2024-08-08 16:52:36 -0400

    Update app.config.js to hide secret key

--- api/config/app.config.js
+++ api/config/app.config.js
@@ -1,5 +1,5 @@
 // const STRIPE_CONFIG = {
-//     SECRET_KEY: "sk_test_51NeMVWGtXGgE8I1fyUpTo3P4dF0GAA2cHKjIYALF013cema2BZtajvsYfhFpS0RKRplnYyC8aB0ou2RzMs3BumKl003EhwhNd4",
+//     SECRET_KEY: "",
 //     CURRENCY: "CAD",
 //     SUCCESS_URL: "http://localhost:4200/success?session_id={CHECKOUT_SESSION_ID}",
 //     CANCEL_URL: "http://localhost:4200/payment"
